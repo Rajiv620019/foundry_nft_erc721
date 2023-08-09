011a6c87ceafe3a773c9cc98bce7884a6ea1599a
Rajiv620019 <rajiv620019@gmail.com>
2023-08-09 22:23:04 +0530

    Wrote test cases to check name & Balance

--- test/DogieNftTest.t.sol
+++ test/DogieNftTest.t.sol
@@ -6,4 +6,31 @@ import {DogieNft} from "../src/DogieNft.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 
-contract DogieNftTest {}
+contract DogieNftTest is Test {
+    string constant NFT_NAME = "Dogie";
+    string constant NFT_SYMBOL = "DOG";
+    DogieNft public basicNft;
+    DeployDogieNft public deployer;
+    address public deployerAddress;
+
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    address public constant USER = address(1);
+
+    function setUp() public {
+        deployer = new DeployDogieNft();
+        basicNft = deployer.run();
+    }
+
+    function testInitializedCorrectly() public view {
+        assert(keccak256(abi.encodePacked(basicNft.name())) == keccak256(abi.encodePacked((NFT_NAME))));
+        assert(keccak256(abi.encodePacked(basicNft.symbol())) == keccak256(abi.encodePacked((NFT_SYMBOL))));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG_URI);
+
+        assert(basicNft.balanceOf(USER) == 1);
+    }
+}
