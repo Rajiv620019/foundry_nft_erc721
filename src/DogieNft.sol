481c01d435d205a82dc4c818c43e4193625546df
Rajiv620019 <rajiv620019@gmail.com>
2023-08-09 20:31:32 +0530

    setup storage variables and mint function

--- src/DogieNft.sol
+++ src/DogieNft.sol
@@ -4,5 +4,16 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DogieNft is ERC721 {
-    constructor() ERC721("Dogie", "DOG") {}
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenURI) public {
+        s_tokenToUri[s_tokenCounter] = tokenURI;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 }
