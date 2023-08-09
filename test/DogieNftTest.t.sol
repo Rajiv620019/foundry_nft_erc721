41de0706973ce5f1a8325bb2bc34151d19a7bd71
Rajiv620019 <rajiv620019@gmail.com>
2023-08-09 22:15:09 +0530

    Did some changes

---
+++ test/DogieNftTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {DeployDogieNft} from "../script/DeployDogieNft.s.sol";
+import {DogieNft} from "../src/DogieNft.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+
+contract DogieNftTest {}
