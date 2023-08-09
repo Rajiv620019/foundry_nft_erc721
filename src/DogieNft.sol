38b030bbb629832a0fa961a70af00df78ff65808
Rajiv620019 <rajiv620019@gmail.com>
2023-08-09 20:36:25 +0530

    implemented tokenUri

--- src/DogieNft.sol
+++ src/DogieNft.sol
@@ -16,4 +16,8 @@ contract DogieNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter++;
     }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return s_tokenToUri[tokenId];
+    }
 }
